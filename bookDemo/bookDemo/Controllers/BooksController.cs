d64941b49d8e26dc3c2b2fdb7ab2114d5f91a297
Rojhat Çetin <201903327+rjhtctn@users.noreply.github.com>
2025-07-27 19:58:55 +0300

    Modeller ile çalışma.

--- bookDemo/bookDemo/Controllers/BooksController.cs
+++ bookDemo/bookDemo/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using bookDemo.Data;
+using bookDemo.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace bookDemo.Controllers
 {
@@ -26,5 +28,82 @@ namespace bookDemo.Controllers
             }
             return Ok(book);
         }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book == null) return BadRequest();
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(200, book);
+            }
+            catch
+            (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            var entity = ApplicationContext.Books
+                .Find(b => b.Id.Equals(id));
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            ApplicationContext.Books.Remove(entity);
+            book.Id = entity.Id;
+            ApplicationContext.Books.Add(book);
+            return Ok(book);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAllBooks()
+        {
+            ApplicationContext.Books.Clear();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")]int id)
+        {
+            var entity = ApplicationContext.Books
+                .Find(b => b.Id.Equals(id));
+            if (entity is null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    message = $"Book with id:{id} could not found."
+                });
+            }
+            ApplicationContext.Books.Remove(entity);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name="id")] int id, 
+            [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+            var entity = ApplicationContext.Books.Find(b => b.Id.Equals(id));
+            if (entity is null)
+            {
+                return NotFound();  
+            }
+            bookPatch.ApplyTo(entity);
+            return NoContent();
+        }
+
     }
 }
--- bookDemo/bookDemo/Program.cs
+++ bookDemo/bookDemo/Program.cs
@@ -9,7 +9,7 @@ namespace bookDemo
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddNewtonsoftJson();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -17,7 +17,7 @@ namespace bookDemo
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())    
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
